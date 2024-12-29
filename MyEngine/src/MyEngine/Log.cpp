717da10acd2709cce97e0fc984dfeefcde99752b
YourEverydayUser <f.kuster13@gmail.com>
2024-12-29 21:29:24 +0100

    Logging

--- MyEngine/src/MyEngine/Log.cpp
+++ MyEngine/src/MyEngine/Log.cpp
@@ -2,11 +2,16 @@
 
 namespace MyEngine {
 
-	Log::Log() {
+	std::shared_ptr<spdlog::logger> Log::s_CoreLogger;
+	std::shared_ptr<spdlog::logger> Log::s_ClientLogger;
 
-	}
+	void Log::Init() {
+		//initialize the logger with the timestamp (T), the name of the logger (n) and the actual message (v)
+		spdlog::set_pattern("%^[%T] %n: %v%$");
+		s_CoreLogger = spdlog::stdout_color_mt("MYENGINE");
+		s_CoreLogger->set_level(spdlog::level::trace);
 
-	Log::~Log() {
-
+		s_ClientLogger = spdlog::stderr_color_mt("APP");
+		s_ClientLogger->set_level(spdlog::level::trace);
 	}
 }
