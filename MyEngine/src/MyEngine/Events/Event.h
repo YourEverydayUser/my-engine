d5b44ed0a2cd1f5e5e63cff4cb427bf5d10e1980
YourEverydayUser <f.kuster13@gmail.com>
2025-01-04 12:57:02 +0100

    EventDispatcher

--- MyEngine/src/MyEngine/Events/Event.h
+++ MyEngine/src/MyEngine/Events/Event.h
@@ -51,6 +51,20 @@ namespace MyEngine {
 		template<typename T>
 		using EventFn = std::function<bool(T&)>;
 
+	public:
+		EventDispatcher(Event& event)
+			: m_Event(event) {}
 
+		template <typename T>
+		bool Dispatch(EventFn<T> func) {
+			if (m_Event.GetEventType() == T::GetStaticType()) {
+				m_Event.m_Handled = func(*(T*)&m_Event);
+				return true;
+			}
+			return false;
+		}
+
+	private:
+		Event& m_Event;
 	};
 }
